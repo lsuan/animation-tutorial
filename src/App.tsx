90c985c71a28cc482fb881911d8b8e837f354b08
Lee Suan <leesuan97@gmail.com>
2023-02-08 00:53:23 -0800

    finish simple animation

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <button className="square" />
+        <button className="square" />
+        <button className="square" />
+        <button className="square" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
